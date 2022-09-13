4c1e9339c4eb794f5dedfaaeb901a52e32083e64
kerni714 <kerni714@student.liu.se>
2022-09-13 19:08:56 +0200

    Add data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Grap: edges and weights
+#'
+#' A dataset containing the edges and weights of a graph.
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{node, egde pair 1}
+#'   \item{v2}{node, edge pair 2}
+#'   \item{w}{weight of the edge}
+#'   ...
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm/}
+"wiki_graph"
